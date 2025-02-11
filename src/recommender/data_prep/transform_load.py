897143d0f53b97cf135bb7c40f1fa7d12c1e7003
Het Parekh <hetparekh16@gmail.com>
2025-02-11 21:42:35 +0100

    drop rows with missing values and duplicates

--- src/recommender/data_prep/transform_load.py
+++ src/recommender/data_prep/transform_load.py
@@ -5,3 +5,16 @@ bollywood_movies = pd.read_csv(BASE_PATH / "data/raw_data/bollywood_movies.csv")
 hollywood_movies = pd.read_csv(BASE_PATH / "data/raw_data/hollywood_movies.csv")
 bollywood_series = pd.read_csv(BASE_PATH / "data/raw_data/bollywood_series.csv")
 hollywood_series = pd.read_csv(BASE_PATH / "data/raw_data/hollywood_series.csv")
+
+bollywood_movies.drop(columns=["Writer", "Producer"], inplace=True)
+hollywood_movies.drop(columns=["Writer", "Producer"], inplace=True)
+
+bollywood_movies.dropna(inplace=True)
+hollywood_movies.dropna(inplace=True)
+bollywood_series.dropna(inplace=True)
+hollywood_series.dropna(inplace=True)
+
+bollywood_movies.drop_duplicates(inplace=True)
+hollywood_movies.drop_duplicates(inplace=True)
+bollywood_series.drop_duplicates(inplace=True)
+hollywood_series.drop_duplicates(inplace=True)
