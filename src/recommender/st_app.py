4512bd404041c3bf6244c52b31fdca012f028ebf
Het Parekh <hetparekh16@gmail.com>
2025-02-11 22:09:00 +0100

    Loaded movies and series data in streamlit app

--- src/recommender/st_app.py
+++ src/recommender/st_app.py
@@ -9,3 +9,17 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+
+@st.cache_data
+def load_data():
+    movies = BASE_PATH / "data/processed_data/movies.csv"
+    series = BASE_PATH / "data/processed_data/series.csv"
+    return (
+        pd.read_csv(movies),
+        pd.read_csv(series),
+    )
+
+
+movies = load_data()[0]
+series = load_data()[1]
