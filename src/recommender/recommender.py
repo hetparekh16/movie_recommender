dd2bdb4947f6a8b142251cb7ea380c04f664faad
Het Parekh <hetparekh16@gmail.com>
2025-02-11 21:57:52 +0100

    Initialized the model and created embeddings and similarity scores

--- src/recommender/recommender.py
+++ src/recommender/recommender.py
@@ -9,3 +9,11 @@ movies["Tags"] = (
     movies["Description"] + " " + movies["Genres"] + " " + movies["Director"]
 )
 series["Tags"] = series["Description"] + " " + series["Genres"]
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+movie_embeddings = model.encode(movies["Tags"].tolist())
+series_embeddings = model.encode(series["Tags"].tolist())
+
+movie_similarity = model.similarity(movie_embeddings, movie_embeddings)
+series_similarity = model.similarity(series_embeddings, series_embeddings)
