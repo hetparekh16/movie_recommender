c533f40724ff073907b0d88274ad38f8128558e7
Het Parekh <hetparekh16@gmail.com>
2025-02-11 23:28:07 +0100

    Added parallel fetching of genre

--- src/recommender/data_prep/retriever.py
+++ src/recommender/data_prep/retriever.py
@@ -9,3 +9,13 @@ import chromadb
 load_dotenv()
 chroma_client = chromadb.PersistentClient(path=f"{BASE_PATH}/data")
 collection = chroma_client.get_or_create_collection(name="movies")
+
+
+async def fetch_genre(language):
+    async with aiohttp.ClientSession() as session:
+        params = {"api_key": os.getenv("API_KEY"), "language": language}
+        async with session.get(
+            f"{BASE_URL}/genre/movie/list", params=params
+        ) as response:
+            data = await response.json()
+            return {genre["id"]: genre["name"] for genre in data.get("genres", [])}
