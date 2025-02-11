0b93613f83feb85581f7592550e3b0e4eb49e3d6
Het Parekh <hetparekh16@gmail.com>
2025-02-11 23:27:10 +0100

    Loaded envirnment variable and initialized chroma client

--- src/recommender/data_prep/retriever.py
+++ src/recommender/data_prep/retriever.py
@@ -5,3 +5,7 @@ import os
 import asyncio
 import aiohttp
 import chromadb
+
+load_dotenv()
+chroma_client = chromadb.PersistentClient(path=f"{BASE_PATH}/data")
+collection = chroma_client.get_or_create_collection(name="movies")
