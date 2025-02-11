aa08dea77ede032eb88752a654dbd28d7676f51e
Het Parekh <hetparekh16@gmail.com>
2025-02-11 22:52:09 +0100

    Renamed the package

--- src/recommender/st_app.py
+++ src/recommender/st_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from recommender.recommender import recommend_movies, recommend_series
+from recommender.main import recommend_movies, recommend_series
 from recommender.config import BASE_PATH
 import pandas as pd
 
