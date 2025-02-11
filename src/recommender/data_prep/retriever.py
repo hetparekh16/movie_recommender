9b90671e960bebfbb846845150050209e1d88b23
Het Parekh <hetparekh16@gmail.com>
2025-02-11 23:30:51 +0100

    Storing movies in chroma db

--- src/recommender/data_prep/retriever.py
+++ src/recommender/data_prep/retriever.py
@@ -42,3 +42,39 @@ async def fetch_movies(total_pages, language):
         results = await asyncio.gather(*tasks)
         movies = [movie for page in results for movie in page]
     return movies
+
+
+async def store_movies(total_pages, language):
+    movies = await fetch_movies(total_pages, language)
+    genre_mapping = await fetch_genre(language)
+    for movie in movies:
+        collection.add(
+            ids=str(movie["id"]),
+            documents=[
+                movie["overview"] if movie["overview"] else "No description available"
+            ],
+            metadatas=[
+                {
+                    "adult": movie["adult"],
+                    "genres": ", ".join(
+                        [
+                            genre_mapping.get(genre_id, "Unknown")
+                            for genre_id in movie.get("genre_ids", [])
+                        ]
+                    ),
+                    "original_language": movie["original_language"],
+                    "original_title": movie["original_title"],
+                    "overview": movie["overview"],
+                    "popularity": movie["popularity"],
+                    "poster_path": (
+                        f"https://image.tmdb.org/t/p/w500{('poster_path')}"
+                        if movie.get("poster_path")
+                        else ""
+                    ),
+                    "release_date": movie["release_date"],
+                    "title": movie["title"],
+                    "vote_average": movie["vote_average"],
+                    "vote_count": movie["vote_count"],
+                }
+            ],
+        )
