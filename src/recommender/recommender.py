c36d77e3ae70a633cdea474276fe6e6ef0cb5fac
Het Parekh <hetparekh16@gmail.com>
2025-02-11 21:57:14 +0100

    Created tags for embeddings

--- src/recommender/recommender.py
+++ src/recommender/recommender.py
@@ -4,3 +4,8 @@ from sentence_transformers import SentenceTransformer
 
 movies = pd.read_csv(BASE_PATH / "data/processed_data/movies.csv")
 series = pd.read_csv(BASE_PATH / "data/processed_data/series.csv")
+
+movies["Tags"] = (
+    movies["Description"] + " " + movies["Genres"] + " " + movies["Director"]
+)
+series["Tags"] = series["Description"] + " " + series["Genres"]
