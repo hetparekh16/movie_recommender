ac61cba74d7c2741ba6b3256ff0605c71dfbc0d7
Het Parekh <hetparekh16@gmail.com>
2025-02-11 22:08:14 +0100

    Set page config for streamlit app

--- src/recommender/st_app.py
+++ src/recommender/st_app.py
@@ -2,3 +2,10 @@ import streamlit as st
 from recommender.recommender import recommend_movies, recommend_series
 from recommender.config import BASE_PATH
 import pandas as pd
+
+st.set_page_config(
+    page_title="Movie Recommender System",
+    page_icon="🎬",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
